bef6981b1884089f8c18ce8edc6e9c145c5f325b
Git Automerge <jcguarinpenaranda@gmail.com>
2018-02-17 19:59:20 -0500

    created CachedResults class

---
+++ src/CachedResults.ts
@@ -0,0 +1,36 @@
+import * as Parse from 'parse';
+
+export default class CachedResults {
+  createdAt: Date;
+  results: any[];
+
+  constructor(objs: Parse.Object[], createdAt?: Date) {
+    this.createdAt = createdAt || new Date();
+    this.results = objs.map(o => o.toJSON());
+  }
+
+  toJSON(): any {
+    return {
+      createdAt: this.createdAt,
+      resuts: this.results,
+    };
+  }
+
+  toParseObjs(className: string): Parse.Object[] {
+    return this.results.map((obj) => {
+      const jsonReadyToBeConverted = {
+        className,
+        ...obj,
+      };
+
+      // return Parse objects
+      return Parse.Object.fromJSON(jsonReadyToBeConverted, false);
+    });
+  }
+
+  static fromJSON(
+    json: { createdAt: Date, results: any[] },
+  ): CachedResults {
+    return new CachedResults(json.results, json.createdAt);
+  }
+}
