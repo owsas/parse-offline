3dc1e34ff9c9010e877676b58bfd65b6f7f1fa46
Git Automerge <jcguarinpenaranda@gmail.com>
2018-02-17 19:04:46 -0500

    starting to program

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+import * as Parse from 'parse';
+
 export class MyModule {
-  
+  static async saveResultsToLocalStorage(
+    query: Parse.Query,
+    localStorageKey?: string,
+  ): Promise<Parse.Object[]> {
+    
+  }
 }
